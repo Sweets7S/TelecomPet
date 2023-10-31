de37309ab8d75dd6add461b7d382b8480d10b22a
Yagorick <EgorKarpikhin@yandex.ru>
2023-10-31 15:43:38 +0300

    --add README.md

--- src/main/java/ru/fintech/example/TelecomApplication.java
+++ src/main/java/ru/fintech/example/TelecomApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ExampleApplication {
+public class TelecomApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ExampleApplication.class, args);
+        SpringApplication.run(TelecomApplication.class, args);
     }
 }
