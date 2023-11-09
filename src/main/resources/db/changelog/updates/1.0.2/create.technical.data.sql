7e2212c85045bd798529d05919361d9c0d866804
Yagorick <EgorKarpikhin@yandex.ru>
2023-11-09 17:06:48 +0300

    --builds

---
+++ src/main/resources/db/changelog/updates/1.0.2/create.technical.data.sql
@@ -0,0 +1,8 @@
+INSERT INTO users (login, password, fio, document, active)
+VALUES ('technical', 'technical', 'technical', 'technical', true);
+
+INSERT INTO tariff (name, price_per_month, package_voice, package_data, package_sms, speed_max, package_voice_country)
+VALUES ('technical', 1, 1, 1, 1, 1, 1);
+
+INSERT INTO options (name, price_per_month, package_voice, package_data, package_sms, spec_code)
+VALUES ('technical', 1, 1, 1, 1, 1);
